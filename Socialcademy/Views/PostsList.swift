cc179ebe70d2cb8033a38cbb38f50066709f7b9f
Anastasija Kipcevic <akiptsevich@gmail.com>
2024-04-10 17:39:01 +0200

    Download posts from Firestore and update the UI and previews.

--- Socialcademy/Views/PostsList.swift
+++ Socialcademy/Views/PostsList.swift
@@ -15,13 +15,32 @@ struct PostsList: View {
     
     var body: some View {
         NavigationView {
-            List(viewModel.posts) { post in
-                if searchText.isEmpty || post.contains(searchText) {
-                    PostRow(post: post)
+            Group {
+                switch viewModel.posts {
+                case .loading:
+                    ProgressView()
+                case let .error(error):
+                    EmptyListView(
+                        title: "Cannot Load Posts",
+                        message: error.localizedDescription,
+                        retryAction: {
+                            viewModel.fetchPosts()
+                        })
+                case .empty:
+                    EmptyListView(
+                        title: "No Posts",
+                        message: "There aren't any posts yet."
+                    )
+                case let .loaded(posts):
+                    List(posts) { post in
+                        if searchText.isEmpty || post.contains(searchText) {
+                            PostRow(post: post)
+                        }
+                    }
+                    .searchable(text: $searchText)
                 }
             }
             .navigationTitle("Posts")
-            .searchable(text: $searchText)
             .toolbar {
                 Button {
                     showNewPostForm = true
@@ -33,11 +52,30 @@ struct PostsList: View {
                 NewPostForm(createAction: viewModel.makeCreateAction())
             }
         }
+        .onAppear {
+            viewModel.fetchPosts()
+        }
     }
 }
 
+#if DEBUG
 struct PostsList_Previews: PreviewProvider {
+    @MainActor
+    private struct ListPreview: View {
+        let state: Loadable<[Post]>
+        
+        var body: some View {
+            let postsRepository = PostsRepositoryStub(state: state)
+            let viewModel = PostsViewModel(postsRepository: postsRepository)
+            PostsList(viewModel: viewModel)
+        }
+    }
+    
     static var previews: some View {
-        PostsList()
+        ListPreview(state: .loaded([Post.testPost]))
+        ListPreview(state: .empty)
+        ListPreview(state: .error)
+        ListPreview(state: .loading)
     }
 }
+#endif
